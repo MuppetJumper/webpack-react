a1c240dc8242f8f72b152754bb5473859d296eea
Yuval Cohen <muppetjumper@gmail.com>
2018-04-04 10:36:06 +0300

    Redesign objectives list - add prompt of current year

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import '../css/objectives.css';
 
 const objectiveList = (
-  <ul className = "objectives" >
-    <li>Learn React</li>
-    <li>Learn web designing</li>
-    <li>Study coding riddles</li>
-    <li>Improve interview skills</li>
-    <li>Build several portfolio web-apps</li>
-  </ul>
+  <div className = "objectives">
+    <p>
+      <i>According to the year {new Date().getFullYear()} my goals are as follows:</i>
+    </p>
+    <ul>
+      <li>Learn React</li>
+      <li>Learn web designing</li>
+      <li>Study coding riddles</li>
+      <li>Improve interview skills</li>
+      <li>Build several portfolio web-apps</li>
+    </ul>
+  </div>
 );
 
 ReactDOM.render(objectiveList,
