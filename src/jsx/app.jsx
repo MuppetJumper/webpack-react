f98c0fb9d0ccc83a0e1a84e6e5980191c392ecf3
Yuval Cohen <muppetjumper@gmail.com>
2018-04-05 18:43:09 +0300

    Separate objective list to a React componenet

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './components/profile.jsx';
-import '../css/objectives.css';
-
-
-
-
-
-
-
-
-let isInterviewerNice = (()=>{
-  return ( Math.random() < 0.5 );
-});
-
-const normalObjectives = ['Learn React',
-                          'Learn web designing',
-                          'Study coding riddles',
-                          'Improve interview skills',
-                          'Build several portfolio web-apps'];
-
-const objectivesSegment = (
-  <div className="objectives">
-    <p>
-      <i>According to the year {new Date().getFullYear()} my goals are as follows:</i>
-    </p>
-    <ul>
-      {normalObjectives.map(objective=><li>{objective}</li>)}
-      { isInterviewerNice() && <li>Learn to cook</li> }
-    </ul>
-  </div>
-);
+import Objectives from './components/objectives.jsx';
 
 const appLayout = (
   <div>
     <Profile />
-    {objectivesSegment}
+    <Objectives />
   </div>
 );
 
---
+++ src/jsx/components/objectives.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../../css/objectives.css';
+
+export default class Objectives extends React.Component {
+
+  isInterviewerNice(){
+    return ( Math.random() < 0.5 );
+  }
+  constructor() {
+    super();
+
+    this.normalObjectives = ['Learn React',
+                             'Learn web designing',
+                             'Study coding riddles',
+                             'Improve interview skills',
+                             'Build several portfolio web-apps'];
+
+    this.kissAssObjectives = ['Learn to cook'];
+  }
+  render() {
+    return (
+      <div className="objectives">
+        <p>
+          <i>According to the year {new Date().getFullYear()} my goals are as follows:</i>
+        </p>
+        <ul>
+          {this.normalObjectives.map(item => {
+            return <li>{item}</li>;
+          })}
+          {this.isInterviewerNice() &&
+           this.kissAssObjectives.map( item => {
+             return <li>{item}</li>;
+           })}
+        </ul>
+      </div>
+    );
+  }
+}
