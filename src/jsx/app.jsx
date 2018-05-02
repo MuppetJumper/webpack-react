4e101b85447ef7e42dffa3085f1000b4ea4d8037
Yuval Cohen <muppetjumper@gmail.com>
2018-05-02 16:42:04 +0300

    Add Stock status to app

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './components/profile.jsx';
 import Objectives from './components/objectives.jsx';
+import {StockStatusContainer} from './containers/StockStatusContainer.jsx';
 
 const appLayout = (
   <div>
     <Profile />
     <Objectives nicePercent='50' />
+    <StockStatusContainer />
   </div>
 );
 
